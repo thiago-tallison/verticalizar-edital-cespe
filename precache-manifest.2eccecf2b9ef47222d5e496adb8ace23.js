d650a466e94ae09fe3078bc63c5daa3781b53933
Thiago Tallison <thiagotallison@gmail.com>
2020-06-08 20:42:16 -0300

    Updates

---
+++ precache-manifest.2eccecf2b9ef47222d5e496adb8ace23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65960e633e9177b5a9ad917cc2ce5463",
+    "url": "/verticalizar-edital-cespe/index.html"
+  },
+  {
+    "revision": "b95342a8eebcd33717be",
+    "url": "/verticalizar-edital-cespe/static/css/main.29994917.chunk.css"
+  },
+  {
+    "revision": "eebcb9633254614f3578",
+    "url": "/verticalizar-edital-cespe/static/js/2.5006a9c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/verticalizar-edital-cespe/static/js/2.5006a9c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b95342a8eebcd33717be",
+    "url": "/verticalizar-edital-cespe/static/js/main.b8be6486.chunk.js"
+  },
+  {
+    "revision": "9147b2ff36d5a214cfc3",
+    "url": "/verticalizar-edital-cespe/static/js/runtime-main.c2506851.js"
+  },
+  {
+    "revision": "50ab59f9106a5487e0c7b510cd3fccf8",
+    "url": "/verticalizar-edital-cespe/static/media/icons8-github.50ab59f9.svg"
+  }
+]);
