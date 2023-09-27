9bc6e2b247c640cd766b186049d2fed9c67cac5b
Thiago Tallison <thiagotallison@gmail.com>
2023-09-27 10:49:32 -0300

    chore: add chadcnui

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
-import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
-import viteTsconfigPaths from "vite-tsconfig-paths";
-import svgrPlugin from "vite-plugin-svgr";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "build",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-  plugins: [
-    react({
-      jsxRuntime: "classic", // Add this line
-    }),
-    viteTsconfigPaths(),
-    svgrPlugin(),
-  ],
 });
