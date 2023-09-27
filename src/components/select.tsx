6617983120bb2c03d5a3e3d19c7e0a923d21e005
Thiago Tallison <thiagotallison@gmail.com>
2023-09-27 12:59:24 -0300

    feat: create select component

---
+++ src/components/select.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import {
+  Select as ChadSelect,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+type SelectItemType = {
+  value: string;
+  label: string;
+};
+
+type Props = {
+  id: string;
+  label: string;
+  placeholder: string;
+  items: SelectItemType[];
+};
+
+export function Select({ items, placeholder }: Props) {
+  return (
+    <ChadSelect>
+      <SelectTrigger>
+        <SelectValue placeholder={placeholder} />
+      </SelectTrigger>
+      <SelectContent>
+        {items.map((item) => (
+          <SelectItem key={item.value} value={item.value}>
+            {item.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </ChadSelect>
+  );
+}
